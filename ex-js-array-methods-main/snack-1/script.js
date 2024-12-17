e84f36332e409d2f56da63b645f8412971cca95f
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-17 12:24:42 +0100

    feat : add first Snack

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+names.forEach(function(name, index) {
+       
+    console.log(`index: ${index} , Names: ${name}`);
+});
