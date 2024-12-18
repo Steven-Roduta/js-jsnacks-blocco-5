75343e57926b25031e786b02cf20088810ffb0b7
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-18 13:23:32 +0100

    feat: add third snack

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,9 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+numbers.forEach(function(num, index, array) {
+    array[index] = num + 1;
+});
+
+console.log(numbers);
 // Risultato: [3, 9, 5, 8, 3, 88]
